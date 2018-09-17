3e4eb882341168efa2c1daca7f4e51733dde31d0
mohong <arthas_qin@qq.com>
2018-09-17 21:37:16 +0800

    init project

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  base: '/computer-network/',
+  title: '数据结构',
+  description: 'Just playing around',
+  themeConfig: {
+    sidebar: [
+      ['/', '概述'],
+      {
+        title: '数组',
+        collapsable: false,
+        children: []
+      }
+    ]
+  }
+}
