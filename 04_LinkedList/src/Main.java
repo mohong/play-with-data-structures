3322d80a90860b947066555e4d14c08d706f27dc
mohong <arthas_qin@qq.com>
2018-09-22 09:30:20 +0800

    完成链表基础结构

---
+++ 04_LinkedList/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+        linkedList.addLast(1);
+        linkedList.addLast(2);
+        linkedList.addLast(3);
+        linkedList.addLast(4);
+
+        linkedList.addFirst(0);
+
+        System.out.println(linkedList.toString());
+        System.out.println("是否为空：" + linkedList.isEmpty());
+
+    }
+}
