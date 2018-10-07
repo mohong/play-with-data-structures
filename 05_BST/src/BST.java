b5c451ef15dec1f6b742be7bebd90de8db4da084
mohong <arthas_qin@qq.com>
2018-10-07 23:09:57 +0800

    增加删除任意节点

--- 05_BST/src/BST.java
+++ 05_BST/src/BST.java
@@ -258,6 +258,60 @@ public class BST<E extends Comparable<E>> {
         return node;
     }
 
+    /**
+     * 从二分搜索树中删除元素为e的节点
+     * @param e
+     */
+    public void remove(E e) {
+        root = remove(root, e);
+    }
+
+    // 删除掉以node为根的二分搜索树中值为e的节点, 递归算法
+    // 返回删除节点后新的二分搜索树的根
+    private Node remove(Node node, E e) {
+        if( node == null ) {
+            return null;
+        }
+        if( e.compareTo(node.e) < 0 ){
+            node.left = remove(node.left , e);
+            return node;
+        }
+        else if(e.compareTo(node.e) > 0 ){
+            node.right = remove(node.right, e);
+            return node;
+        }
+        else{   // e.compareTo(node.e) == 0
+
+            // 待删除节点左子树为空的情况
+            if(node.left == null){
+                Node rightNode = node.right;
+                node.right = null;
+                size --;
+                return rightNode;
+            }
+
+            // 待删除节点右子树为空的情况
+            if(node.right == null){
+                Node leftNode = node.left;
+                node.left = null;
+                size --;
+                return leftNode;
+            }
+
+            // 待删除节点左右子树均不为空的情况
+
+            // 找到比待删除节点大的最小节点, 即待删除节点右子树的最小节点
+            // 用这个节点顶替待删除节点的位置
+            Node successor = minimum(node.right);
+            successor.right = removeMin(node.right);
+            successor.left = node.left;
+
+            node.left = node.right = null;
+
+            return successor;
+        }
+    }
+
     @Override
     public String toString(){
         StringBuilder res = new StringBuilder();
