3523ea667edc36af769eb07c2c00e828838713f3
mohong <arthas_qin@qq.com>
2018-10-07 23:02:36 +0800

    增加删除最大值和最小值的方法

--- 05_BST/src/BST.java
+++ 05_BST/src/BST.java
@@ -212,6 +212,52 @@ public class BST<E extends Comparable<E>> {
         return maxmum(node.right);
     }
 
+    /**
+     * 从二分搜索树中删除最小值所在节点, 返回最小值
+     * @return
+     */
+    public E removeMin() {
+        E ret = minimum();
+        root = removeMin(root);
+        return ret;
+    }
+
+    // 删除掉以node为根的二分搜索树中的最小节点
+    // 返回删除节点后新的二分搜索树的根
+    private Node removeMin(Node node) {
+        if (node.left == null) {
+            Node rightNode = node.right;
+            node.right = null;
+            size --;
+            return rightNode;
+        }
+        node.left = removeMin(node.left);
+        return node;
+    }
+
+    /**
+     * 从二分搜索树中删除最大值所在节点
+     * @return
+     */
+    public E removeMax() {
+        E ret = maxmum();
+        root = removeMax(root);
+        return ret;
+    }
+
+    // 删除掉以node为根的二分搜索树中的最大节点
+    // 返回删除节点后新的二分搜索树的根
+    private Node removeMax(Node node) {
+        if (node.right == null) {
+            Node leftNode = node.left;
+            node.left = null;
+            size--;
+            return leftNode;
+        }
+        node.right = removeMax(node.right);
+        return node;
+    }
+
     @Override
     public String toString(){
         StringBuilder res = new StringBuilder();
